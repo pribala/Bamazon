6f053250808146629917ecfe7a663479738a6584
Priya Balakrishnan <andu_pri@yahoo.com>
2017-10-15 22:50:25 -0400

    feat: Update products table on successful purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,12 +26,60 @@ connection.query('SELECT item_id, product_name, price FROM products', function (
   // results will contain the results of the query
   // fields will contain information about the returned results fields (if any)
   if(error) throw error;
+  //var data = JSON.stringify(results);
   console.log("\n=====================================================================\n");
-  console.log("Product Id     Product Name         Price");
+  console.log("Items Available");
+  console.log("----------------");
   results.forEach(function(item){
-    console.log(item.item_id, item.product_name, item.price);
+    console.log("Id: "+ item.item_id + " || Name: " + item.product_name +" || Price: "+ item.price);
   });
   console.log("\n=====================================================================\n");  
-  connection.end();
+  
+  inquirer
+    .prompt([{
+      name: "id",
+      type: "input",
+      message: "Enter the Id of the product you would like to purchase."
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "Enter the quantity you would like to purchase."
+    }
+    ]).then(function(answer) {
+      updateQuantity(answer.id, answer.quantity);
+    });
+  //connection.end();
 });
 }
+
+function updateQuantity(id, quantity) {
+  var query = "SELECT stock_quantity, price FROM products WHERE ?";
+  connection.query(query, { item_id: id }, function(err, res) {
+    res.forEach(function(item){
+      if(item.stock_quantity < quantity){
+        console.log("Insufficient quantity!");
+      }else {
+        var newQuantity = item.stock_quantity - quantity;
+        var query = "UPDATE products SET ? WHERE ?";
+        connection.query(query,
+        [
+          {
+            stock_quantity: quantity
+          },
+          {
+            item_id: id
+          }
+        ],
+        function(err, result) {
+          if (err) throw err;
+          //console.log(quantity + " rows updated!\n");
+          res.forEach(function(item){
+            console.log("Total cost of purchase: "+ item.price*quantity);
+          });
+        });
+      }
+    });
+  });  
+}
+
