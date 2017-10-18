539d5ddd4b2f458860637c6df5a8fbb4123fb288
Priya Balakrishnan <andu_pri@yahoo.com>
2017-10-18 00:57:47 -0400

    feat: CRUD operations on products database

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,6 @@
 var mysql      = require('mysql');
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -45,30 +46,42 @@ inquirer
     });
 
     function listItems() {
+      var table = new Table({
+        head: ['Item Id', 'Product Name', 'Price', 'Stock Quantity']
+        , colWidths: [15, 60, 15, 20]
+      }); 
     	connection.query('SELECT item_id, product_name, price, stock_quantity FROM products', function (error, results, fields) {
 	    // error will be an Error if one occurred during the query
 	    // results will contain the results of the query
 	    // fields will contain information about the returned results fields (if any)
 	    if(error) throw error;
-	    console.log("\n=====================================================================\n");
 	    console.log("Products for Sale");
 	    console.log("------------------");
 	    results.forEach(function(item){
-	    	console.log("Id: "+ item.item_id + " || Name: " + item.product_name +" || Price: $"+ item.price+" || Quantity: "+item.stock_quantity);
+	    	//console.log("Id: "+ item.item_id + " || Name: " + item.product_name +" || Price: $"+ item.price+" || Quantity: "+item.stock_quantity);
+        table.push([item.item_id, item.product_name, "$"+item.price, item.stock_quantity]);
 	    });
+      console.log(table.toString());
+      connection.end();
 	   });  
     }
 
     function listLowInventory(){
+      var table = new Table({
+        head: ['Item Id', 'Product Name', 'Stock Quantity']
+        , colWidths: [15, 65, 20]
+      }); 
     	var query = "SELECT item_id, product_name, stock_quantity FROM products WHERE stock_quantity < 5";
     	connection.query(query, function(err, res) {
     		if(err) throw error;
-	    	console.log("\n=====================================================================\n");
-	    	console.log("Low Inventory");
-	    	console.log("----------------");
+	    	console.log("Products Low on Inventory");
+	    	console.log("--------------------------");
 	    	res.forEach(function(item){
-	    		console.log("Id: "+ item.item_id + " || Name: " + item.product_name + " || Quantity: "+item.stock_quantity);
-	    	});
+	    		//console.log("Id: "+ item.item_id + " || Name: " + item.product_name + " || Quantity: "+item.stock_quantity);
+	    	  table.push([item.item_id, item.product_name, item.stock_quantity]);
+        });
+        console.log(table.toString());
+        connection.end();
     	});	
     }
 
@@ -104,11 +117,9 @@ inquirer
 				connection.query("SELECT stock_quantity FROM products WHERE item_id = ?", [parseInt(answer.productId)], function(err, res){
 					if(err) throw err;
 					itemQuantity = res[0].stock_quantity;
-					
-				});
-				console.log(itemQuantity);
-				var newQuantity = parseInt(itemQuantity) + parseInt(answer.quantity);
-				console.log(newQuantity);
+						
+				  var newQuantity = parseInt(itemQuantity) + parseInt(answer.quantity);
+				
     			var query = connection.query("UPDATE products SET ? WHERE ?", 
     				[
 				      {
@@ -121,8 +132,10 @@ inquirer
     			 function(err, res) {
 		      		if (err) throw err;
 		      		console.log(res.affectedRows + " product updated!\n");
+              listItems();
 		    	});	
 			 });	
+      });  
     }
 
     function addProduct() {
@@ -172,10 +185,12 @@ inquirer
     				department_name: answer.department,
     				price: parseFloat(answer.price),
     				stock_quantity: parseInt(answer.quantity)};
-    			var query = ("INSERT INTO products SET ?", newItem, function(err, res) {
+    			var query = connection.query("INSERT INTO products SET ?", newItem, function(err, res) {
 		      		if (err) throw err;
 		      		console.log(res.affectedRows + " product inserted!\n");
+              //connection.end();
+              listItems();
 		    	});	
-		   	});
+       	});
     }
 
